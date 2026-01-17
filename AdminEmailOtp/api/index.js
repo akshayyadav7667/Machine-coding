bb3e7d48eb331da4c1bedf11faff99a66410f2f4
Akshay Kumar <80337826+akshayyadav7667@users.noreply.github.com>
2026-01-17 18:43:55 +0530

    Enable CORS with additional methods and headers

--- AdminEmailOtp/api/index.js
+++ AdminEmailOtp/api/index.js
@@ -8,12 +8,24 @@ dotenv.config();
 const app = express();
 
 app.use(express.json());
+// app.use(
+//   cors({
+//     origin: "https://frontend-admin-otp.onrender.com",
+//     credentials: true,
+//   })
+// );
+
+
 app.use(
   cors({
     origin: "https://frontend-admin-otp.onrender.com",
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+app.options("*", cors());
 const PORT = process.env.PORT || 5000;
 
 connectDb();
